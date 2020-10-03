0199bf280d561fb17cf1de560424c20bb862387d
Moises Limon <moises.m.limon@gmail.com>
2020-10-03 01:34:34 -0700

    Completed Part 2 of CodePath's Flix Lab

---
+++ flix/MovieGridCell.swift
@@ -0,0 +1,17 @@
+//
+//  MovieGridCell.swift
+//  flix
+//
+//  Created by Moises Limon on 10/2/20.
+//  Copyright © 2020 Codepath. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+    
+    
+}
